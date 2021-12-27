1471415730c1950c452da8fc82d08fe2de08db38
MyroslavaVynokur <mirka.vyno@gmail.com>
2021-12-27 12:21:21 +0200

    task done

---
+++ HW1/Perimetr.java
@@ -0,0 +1,24 @@
+package Lesson_1.Task1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Perimetr {
+	  public static void main(String[] args) throws IOException {
+		    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("Please enter the radius");
+		
+		double r = Integer.parseInt(br.readLine()); 
+		double p = 3.14;
+		
+			
+		System.out.println("The perimeter calculation:");
+		System.out.println(p * r * r);
+	
+		System.out.println("The area calculation:");
+		System.out.println(2*p*r);
+		
+}
+}
