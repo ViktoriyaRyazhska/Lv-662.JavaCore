a1d47737da9d33f0606ae07c3900bed1b4160bdc
Oleg Kruts <okruts@mirantis.com>
2021-12-27 14:55:48 +0200

    rename classes by java convention

--- src/hw1/Task_1.java
+++ src/hw1/Task_1.java
@@ -4,16 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class task1 {
-
+public class Task_1 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		double pi = 3.14;
-		
-		System.out.println("This program wll calculate perimeter and area pf the flower bed by entering the radius. \n");
+
+		System.out
+				.println("This program wll calculate perimeter and area pf the flower bed by entering the radius. \n");
 		System.out.printf("Please enter radius: ");
 		double r = Integer.parseInt(br.readLine());
-        
+
 		System.out.printf("\nPerimetr of the flower bed (C): ");
 		System.out.println(pi * (2 * r));
 		System.out.printf("Area of the flower bed (A): ");
--- src/hw1/Task_3.java
+++ src/hw1/Task_3.java
@@ -4,10 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class task3 {
-	  public static void main(String[] args) throws IOException {
+public class Task_3 {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
 		System.out.println("This program wll calculate call cost from USA, Poland and Italy to Ukraine. \n");
 		System.out.println("Calls from the USA to Ukraine cost per minute (UAH per minute)?");
 		int c1 = Integer.parseInt(br.readLine());
@@ -21,12 +21,12 @@ public class task3 {
 		int c3 = Integer.parseInt(br.readLine());
 		System.out.println("How many minutes will call contiuned from Italy to Ukraine");
 		int t3 = Integer.parseInt(br.readLine());
-        
+
 		System.out.printf("\nCall from USA to Ukraine for " + t1 + " minutes " + "will cost: ");
 		System.out.println(c1 * t1 + " UAH");
 		System.out.printf("Call from Poland to Ukraine for " + t1 + " minutes " + "will cost: ");
 		System.out.println(c2 * t2 + " UAH");
 		System.out.printf("Call from Italy to Ukraine for " + t1 + " minutes " + "will cost: ");
 		System.out.println(c3 * t3 + " UAH");
-	  }
+	}
 }
