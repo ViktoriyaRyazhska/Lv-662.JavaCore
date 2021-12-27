fcc3104c03ecc39a601d8dc4071481d8eafcf29f
MyroslavaVynokur <mirka.vyno@gmail.com>
2021-12-27 12:32:07 +0200

    task done

---
+++ HW1/Address.java
@@ -0,0 +1,17 @@
+package Lesson_1.Task1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Address {
+	  public static void main(String[] args) throws IOException {
+		    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("What is your name?" );
+		String name = br.readLine();
+		System.out.println("Where do you live?, " + name);
+		String address = br.readLine();
+		System.out.println(name + ", your address is " + address );
+	  }
+	  }
