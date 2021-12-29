af771e17a2f33d9a630644076bd0771380bf0c68
MyroslavaVynokur <mirka.vyno@gmail.com>
2021-12-30 01:25:19 +0200

    class dog

---
+++ src/lesson_3_HW/Dog.java
@@ -0,0 +1,43 @@
+package lesson_3_HW;
+
+public class Dog {
+	private String name;
+	private String breed;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBreed() {
+		return breed;
+	}
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Dog(String name, String breed, int age) {
+		super();
+		this.name = name;
+		this.breed = breed;
+		this.age = age;
+	}
+	public Dog() {
+		super();
+	}
+
+	public enum breed {
+		Akita, Terrier, Spaniel
+	}
+	
+
+	}
+	
+
