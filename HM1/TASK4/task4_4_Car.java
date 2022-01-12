79a6a0ad09ffafe9c3db3c3b1ea84486f13476db
Anton Shcherbei <starteran2gmail.com>
2022-01-12 19:22:47 +0200

    reddit HM task 4 car

--- HM1/TASK4/task4_4_Car.java
+++ HM1/TASK4/task4_4_Car.java
@@ -49,25 +49,27 @@ public class task4_4_Car {
 
     public static void main(String[] args) throws IOException {
 
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] yearOfProduction = new int[4];
         for (int i =0;i < yearOfProduction.length; i++){
             System.out.println("pleas enter year of production " + (i+1) + " car");
             yearOfProduction[i]=Integer.parseInt(br.readLine());
         }
+        
+        task4_4_Car[] cars = new task4_4_Car[4];
+        cars[0] = new task4_4_Car("Sedan",yearOfProduction[0],1.8);
+        cars[1] = new task4_4_Car("Sports Car",yearOfProduction[1],2.2);
+        cars[2] = new task4_4_Car("Hatback",yearOfProduction[2],3.0);
+        cars[3] = new task4_4_Car("Minsvan",yearOfProduction[3],1.9);
 
-        task4_4_Car car1 = new task4_4_Car("Sedan",yearOfProduction[0],1.8);
-        task4_4_Car car2 = new task4_4_Car("Sports Car",yearOfProduction[1],2.2);
-        task4_4_Car car3 = new task4_4_Car("Hatback",yearOfProduction[2],3.0);
-        task4_4_Car car4 = new task4_4_Car("Minsvan",yearOfProduction[3],1.9);
-
-        int n = 0;
-        for (int i = 0; i < yearOfProduction.length-1;i++){
-            for (int j =i + 1 ; j < yearOfProduction.length;j++){
-                if(yearOfProduction[i]>yearOfProduction[i+1]){
-                    n = yearOfProduction[i];
-                    yearOfProduction[i] = yearOfProduction[i+1];
-                    yearOfProduction[i+1] = n;
+        task4_4_Car n ;
+        for (int i = 0; i < cars.length-1;i++){
+            for (int j =i + 1 ; j < cars.length;j++){
+                if(cars[i].getYearOfProduction()>cars[i+1].getYearOfProduction()){
+                    n = cars[i] ;
+                    cars[i] = cars[i+1];
+                    cars[i+1] = n;
                 }
             }
         }
