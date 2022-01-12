daa9d3e671a8b3b301c132fa59a40de6ef1576e2
Oleksandr Riabko <gsmprofi@gmail.com>
2022-01-12 14:47:26 +0200

    hw3

--- hw3/AdditionalTasks.java
+++ hw3/AdditionalTasks.java
@@ -1,5 +1,9 @@
 package hw3;
 
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class AdditionalTasks {
 	
 	// Implement power(x, n), which calculates x raised to the power n (i.e., x^n).
@@ -61,12 +65,60 @@ public class AdditionalTasks {
 		return title.toString();
 		
     }
+	
+	/* 
+	  Given a string s representing a valid expression, implement a basic calculator to evaluate it,
+	  and return the result of the evaluation.
+	*/
+	
+	static int calculate(String s) {
+		
+		String stripped = s.replace(" ", "");
+		System.out.println("original: " + stripped);
+		Pattern pattern = Pattern.compile("[(][^()]+[)]");
+				
+		while (stripped.contains("(")) {
+			
+			Matcher matcher = pattern.matcher(stripped);
+			matcher.find();
+			String foundString = stripped.substring(matcher.start(), matcher.end());
+			System.out.println("parentheses found: " + foundString);
+			System.out.println("evaluating found..." + evaluateString(foundString.replaceAll("[()]", "")));
+			stripped = stripped.replace(foundString, evaluateString(foundString.replaceAll("[()]", "")));
+			System.out.println("simplified to: " + stripped);
+		
+		}
+		        
+		return Integer.parseInt(evaluateString(stripped));
+		
+    }
+	
+	// This method evaluates string which consists of "+", "-", and digits ONLY.
+	
+	static String evaluateString(String s) {
+		
+		int result = 0;
+		String edited = s.replace("+-", "-").replace("--", "+");
+		
+		Pattern pattern = Pattern.compile("[+-]*\\d+");
+		Matcher matcher = pattern.matcher(edited);
+		
+		while (matcher.find()) {
+		
+			result += Integer.parseInt(edited.substring(matcher.start(), matcher.end()));
+		
+		}
+		
+		return String.valueOf(result);
+		
+	}
 
 	public static void main(String[] args) {
 		
 		System.out.println(myPow(2, -1));
 		System.out.println(mySqrt(30));
 		System.out.println(convertToTitle(27));
+		System.out.println("( - 2) + (3-(5+5)) = " + calculate("( - 2) + (3-(5+5))"));
 
 	}
 
