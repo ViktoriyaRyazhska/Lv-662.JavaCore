4f375b5011db9256743135ce3c4fb1cc64dec6ad
Oleg Kruts <okruts@mirantis.com>
2022-01-13 21:32:43 +0200

    add century task

--- src/hw4/Century.java
+++ src/hw4/Century.java
@@ -1,2 +1,25 @@
-package hw4;public class Century {
+package hw4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Century {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Integer y = Integer.parseInt(br.readLine());
+        int test = centuryFromYear(y);
+        System.out.println(test);
+
+    }
+
+    static int centuryFromYear(int year) {
+        if (year % 100 == 0) {
+            year = year / 100;
+        } else {
+            year = (year / 100) + 1;
+        }
+        return year;
+    }
 }
