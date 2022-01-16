080ca42e81a3435c5baba2599426403e1d5fee3c
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-01-16 15:41:05 +0200

    create array of Car’s objects and print it

---
+++ src/lesson_5_CW/Appl.java
@@ -0,0 +1,27 @@
+package lesson_5_CW;
+
+public class Appl {
+    public static void main(String[] args) {
+        Car[] cars = new Car[4];
+        cars[0] = new Truck();
+        cars[0].setModel("mazda");
+        cars[0].setMaxSpeed(250);
+        cars[0].setYearOfManufacture(2005);
+        cars[1] = new Truck();
+        cars[1].setModel("Ford");
+        cars[1].setMaxSpeed(180);
+        cars[1].setYearOfManufacture(2010);
+        cars[2] = new Sedan();
+        cars[2].setModel("lexus");
+        cars[2].setMaxSpeed(280);
+        cars[2].setYearOfManufacture(2020);
+        cars[3] = new Sedan();
+        cars[3].setModel("bmw");
+        cars[3].setMaxSpeed(310);
+        cars[3].setYearOfManufacture(2021);
+
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i].toString());
+        }
+    }
+}
