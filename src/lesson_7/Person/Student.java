14f05e57f4db5231a99fe7b2ab0cf54f565839ca
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-01-17 20:18:31 +0200

    create class Student

---
+++ src/lesson_7/Person/Student.java
@@ -0,0 +1,17 @@
+package lesson_7.Person;
+
+public class Student extends Person{
+
+    final static String Type_Person = "Student";
+
+    public Student(String name) {
+        super(name);
+    }
+
+
+    @Override
+    public void print() {
+        System.out.println("I am " + Type_Person);
+
+    }
+}
