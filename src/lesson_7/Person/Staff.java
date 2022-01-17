1d301e4f9834de6286e1644a54b91ba446e78493
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-01-17 20:17:47 +0200

    abstract class Staff

---
+++ src/lesson_7/Person/Staff.java
@@ -0,0 +1,24 @@
+package lesson_7.Person;
+
+abstract public class Staff extends Person {
+
+    int hours;
+    int rate;
+    String name;
+
+    public Staff(String name, int hours, int rate) {
+        super (name);
+        this.hours = hours;
+        this.rate = rate;
+    }
+
+    public abstract int Salary();
+
+    @Override
+    public String toString() {
+        return "Staff: " +
+                "hours=" + hours +
+                ", rate=" + rate +
+                ", name='" + name;
+    }
+}
