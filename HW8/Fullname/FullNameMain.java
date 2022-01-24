e5246a08370cc530540363c9f47c10a6ad087439
Liliya <liliyashyshak@gmail.com>
2022-01-24 15:17:39 +0200

    method

--- HW8/Fullname/FullNameMain.java
+++ HW8/Fullname/FullNameMain.java
@@ -7,8 +7,8 @@ public class FullNameMain {
 		Person s1 = new Student("Tom", "Red", 20, 2);
 		Person s2 = new Student("Tim", "White", 22, 3);
 		
-		System.out.println(s1.info());
-		System.out.println(s2.info());
+		System.out.println(s1.info() + ". " + s1.activity());
+		System.out.println(s2.info() + ". " + s2.activity());
 		
 	}
 }
