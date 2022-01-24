ab32667c8abe017a4f102c01299e715533008bd0
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-01-24 23:10:08 +0200

    Task 2

---
+++ src/lesson_9/ClassWork/Task_2.java
@@ -0,0 +1,52 @@
+package lesson_9.ClassWork;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Map<Integer, String> employeeMap = new HashMap<>();
+        employeeMap.put(34, "Anna");
+        employeeMap.put(23, "Maria");
+        employeeMap.put(12, "Roza");
+        employeeMap.put(2, "Gina");
+        employeeMap.put(4, "Marga");
+        employeeMap.put(5, "Sem");
+        employeeMap.put(61, "John");
+
+        System.out.println("Initial Mappings are: " + employeeMap);
+        System.out.println("Please enter ID");
+        int y = Integer.parseInt(br.readLine());
+
+
+        if (employeeMap.containsKey(y)) {
+            System.out.println(employeeMap.get(y));
+        } else {
+            System.out.println("There is no such employee");
+        }
+
+        System.out.println("Please enter name");
+        String name = br.readLine();
+
+
+        if (employeeMap.containsValue(name)) {
+            for (Map.Entry<Integer, String> entry : employeeMap.entrySet()) {
+                if (name.equals(entry.getValue())) {
+                    System.out.println("ID = " + entry.getKey());
+                }
+            }
+        }
+        else {
+            System.out.println("There is no such employee");
+        }
+
+
+
+    }
+
+}
