614ab8454eaaca84f2561155ad0a3e6bbaf34a2c
Serhii Okhota <sergiy.okhota@gmail.com>
2022-01-28 19:07:23 +0200

    rename

---
+++ HW7/Wrapper.java
@@ -0,0 +1,18 @@
+package HW8;
+
+public class Wrapper<T> {
+    private T value;
+
+    public Wrapper(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        System.out.println(value);
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
