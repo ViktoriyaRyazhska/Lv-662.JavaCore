8ef984f0ef174ef3775dfbed5423cf002b8828eb
Oleg Kruts <okruts@mirantis.com>
2022-02-03 23:17:14 +0200

    add hw9 homework task 1

---
+++ src/hw9/homework1/FiveWords.java
@@ -0,0 +1,21 @@
+package hw9.homework1;
+
+public class FiveWords {
+    public static void main(String[] args) {
+        String fiveWords = "one two three four five";
+        String longestWord = "";
+
+        String[] words = fiveWords.split(" ");
+
+        for (String word : words) {
+            if (word.length() > longestWord.length()) {
+                longestWord = word;
+            }
+        }
+        System.out.print("Longest word is: \"" + longestWord + "\" with " + longestWord.length() + " letters");
+
+        StringBuilder sb = new StringBuilder(words[1]);
+        sb.reverse();
+        System.out.println("\nSecond word (\"" + words[1] + "\") in reverse order: \"" + sb + "\"");
+    }
+}
