5bebc1c8ebec4e02895b04320bf65d803c101078
Oleg Kruts <okruts@mirantis.com>
2022-02-03 19:53:41 +0200

    add hw9 practical task 3

---
+++ src/hw9/practical3/Username.java
@@ -0,0 +1,29 @@
+package hw9.practical3;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Username {
+    private static final Pattern p = Pattern.compile("[a-zA-Z0-9_]{3,15}");
+    public static void validation(String s) {
+        Matcher m = p.matcher(s);
+        if (m.matches()) {
+            System.out.println(s + ": Your name is valid");
+        } else {
+            System.out.println(s + ": Your name is not valid");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter the name: ");
+            String name = scanner.nextLine();
+            validation(name);
+        }
+    }
+}
+
+
