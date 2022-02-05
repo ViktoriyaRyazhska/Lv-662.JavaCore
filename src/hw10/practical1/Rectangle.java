cac00272d793961cd951621f15caf7436af03fda
Oleg Kruts <okruts@mirantis.com>
2022-02-05 17:51:10 +0200

    add hw10 practical task 1

--- src/hw10/practical1/Rectangle.java
+++ src/hw10/practical1/Rectangle.java
@@ -1,9 +1,30 @@
 package hw10.practical1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Rectangle {
-    public static int squareRectangle (int a, int b) {
+    public static int squareRectangle(int a, int b) {
         if (a <= 0 | b <= 0)
-            throw new ArithmeticException();
+            throw new ArithmeticException("\033[0;1m" + "You entered negative number or zero, can't calculate square");
+        System.out.print("Rectangle square = ");
         return a * b;
+    }
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Calculate square for rectangle\n");
+
+        try {
+            System.out.print("Please enter rectangle length: ");
+            int a = Integer.parseInt(br.readLine());
+            System.out.print("Please enter rectangle width: ");
+            int b = Integer.parseInt(br.readLine());
+            System.out.println(squareRectangle(a, b));
+        } catch (NumberFormatException | IOException e) {
+            System.out.println("\033[0;1m" + "You entered non integer number");
+            e.printStackTrace();
+        }
     }
 }
