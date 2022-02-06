3fecc8551cce77f29eb7f021184c0c45d0859ac0
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-02-06 12:54:21 +0200

    1 and 2

--- src/lesson_12/Practical/MyText.java
+++ src/lesson_12/Practical/MyText.java
@@ -1,9 +1,15 @@
 package lesson_12.Practical;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static java.lang.Character.isLetter;
+import static java.lang.constant.ConstantDescs.NULL;
 
 public class MyText {
     public static void main(String[] args) {
@@ -11,54 +17,17 @@ public class MyText {
         BufferedReader br = null;
         String s = null;
         int count = 0;
-        int index = 0;
+        int indexL = 0;
+        int indexS = 0;
 
         ArrayList<String> LineList = new ArrayList<>();
+        ArrayList<Integer> NumberOfChars = new ArrayList<>();
 
         try {
             br = new BufferedReader(new FileReader(fileName));
             while ((s = br.readLine()) != null) {
                 //Read context from file into array of strings
                 LineList.add(s);
-                //convert each string to characters
-                char[] a = s.toCharArray();
-                //count number of characters if they are not null
-                for (int i = 0; i < a.length; i++) {
-                    if ((int) a[i] != 0) {
-                        count++;
-                    }
-                }
-                //find the longest line
-                int largestString = LineList.get(0).length();
-                for (int i = 0; i < LineList.size(); i++) {  // Test which String is the largest
-                    if (LineList.get(i).length() > largestString) {
-                        largestString = LineList.get(i).length();
-                        index = i;
-                    }
-                    System.out.println("Row " + index + "is the largest and is size " + largestString + ": " + LineList.get(index));
-                }
-                //find the shortest line
-                int shortestString = LineList.get(0).length();
-                for (int i = 0; i < LineList.size(); i++) {
-                    if (LineList.get(i).length() < shortestString) {
-                        largestString = LineList.get(i).length();
-                        index = i;
-                    }
-                    System.out.println("Row " + index + "is the shortest and is size " + shortestString + ": " + LineList.get(index));
-                }
-
-                System.out.println();
-                //count and write the number of symbols in every line
-                System.out.println(" The number of chars in line: " + s + " " + count);
-                System.out.println();
-                // find and write only that lines, which consist of word «Sun»
-                String pattern = ("Sun");
-                Pattern p = Pattern.compile(pattern);
-                Matcher m = p.matcher(s);
-
-                while (m.find()) {
-                    System.out.println(s.substring(m.start(), m.end()));
-                }
             }
             br.close();
         } catch (FileNotFoundException e) {
@@ -66,6 +35,43 @@ public class MyText {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        //count and write the number of symbols in every line.
+        for (int i = 0; i < LineList.size(); i++) {
+            NumberOfChars.add(LineList.get(i).replace(" ", "").length());
+            count++;
+            System.out.println("The number of symbols in line " + count + " is - " + NumberOfChars.get(i));
+        }
+
+        int largestString = NumberOfChars.get(0);
+        for (int i = 0; i < NumberOfChars.size(); i++) {
+            if (NumberOfChars.get(i) > largestString) {
+                largestString = NumberOfChars.get(i);
+                indexL = i + 1;
+            }
+        }
+        System.out.println("The largest row has " + largestString + " symbols " + "and the number of row is " + indexL);
+
+        int shortestString = NumberOfChars.get(0);
+        for (int i = 0; i < NumberOfChars.size(); i++) {
+            if (NumberOfChars.get(i) < shortestString || NumberOfChars.get(i) != 0) {
+                shortestString = NumberOfChars.get(i);
+                indexS = i + 1;
+            }
+        }
+        System.out.println("The shortest row has " + shortestString + " symbols " + "and the number of row is " + indexS);
+
+        //count and write the number of symbols in every line
+
+
     }
 
 }
+
+
+
+
+
+
+
+
