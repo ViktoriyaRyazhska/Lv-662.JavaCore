049256a1856aa7dd481c646d423a860752eb4e4f
Myron Usyk <myronusyk@gmail.com>
2022-02-09 14:33:52 +0200

    09.02.2022

--- Lv-662.JavaCore/src/PracticalTasks_2/Appl.java
+++ Lv-662.JavaCore/src/PracticalTasks_2/Appl.java
@@ -3,27 +3,28 @@ package PracticalTasks_2;
 public class Appl {
 
 	public static void main(String[] args) {
+		
 		Employee e1 = new Employee("Vasyl", 3);
-		e1.setHours(160);
+		e1.setHours(16);
 		e1.changeRate(6);
 
 		Employee e2 = new Employee();
 		e2.setName("Solomiya");
 		e2.setRate(5);
 		e2.changeRate(3);
-		e2.setHours(170);
+		e2.setHours(17);
 
-		Employee e3 = new Employee("Myron", 4, 168);
+		Employee e3 = new Employee("Myron", 4, 18);
 
 		// Info about employees
 		System.out.println(e1 + "\n" + e2 + "\n" + e3 + "\n" );
 
 		// Total salary of all employees
-		System.out.println("\nTotal salary of all employees is:\n" + Employee.getTotalSalary());
+		System.out.println(Employee.getTotalSum() + " $");
 
 		// Total salary with bonuses
 		System.out.println("\nTotal salary of all employees with bonuses is:\n" + ((e1.getSalary() + e1.getBonuses())
-				+ (e2.getSalary() + e2.getBonuses()) + (e3.getSalary() + e3.getBonuses())));
+				+ (e2.getSalary() + e2.getBonuses()) + (e3.getSalary() + e3.getBonuses())) + " $");
 
 	}
 
--- Lv-662.JavaCore/src/PracticalTasks_2/Employee.java
+++ Lv-662.JavaCore/src/PracticalTasks_2/Employee.java
@@ -2,32 +2,23 @@ package PracticalTasks_2;
 
 public class Employee {
 	private static float totalSum = 0;
-	private static int count = 0;
-	
-	
+
 	private String name;
 	private int rate;
 	private int hours;
 
-	{
-		count ++;
-	}
-	
 	public Employee() {
-		totalSum = this.rate * this.hours;
 	}
-	
-	public Employee(String n, int r) {
-		this.name = n;
-		this.rate = r;
-		totalSum = r * this.hours;
+
+	public Employee(String name, int rate) {
+		this.name = name;
+		this.rate = rate;
 	}
-	
-	public Employee(String n, int r, int h) {
-		this.name = n;
-		this.rate = r;
-		this.hours = h;
-		totalSum = r * h;
+
+	public Employee(String name, int rate, int hours) {
+		this.name = name;
+		this.rate = rate;
+		this.hours = hours;
 	}
 
 	public String getName() {
@@ -46,34 +37,37 @@ public class Employee {
 		this.rate = rate;
 	}
 
-	public float getHours() {
+	public int getHours() {
 		return hours;
 	}
 
 	public void setHours(int hours) {
 		this.hours = hours;
 	}
-	
+
 	public double getSalary() {
-		totalSum += (rate * hours);
-		return this.rate * this.hours;
+		totalSum += rate * hours;
+		return rate * hours;
 	}
-	public static double getTotalSalary() {
+
+	public static double getTotalSum() {
 		return totalSum;
 	}
+
 	public void changeRate(int rate) {
-//		totalSum -= this.rate;
+		totalSum -= this.rate;
 		this.rate = rate;
 		totalSum += rate;
 	}
+
 	public double getBonuses() {
-		return (rate * hours * 0.1);
+		return getSalary() * 0.1;
 	}
 
 	@Override
 	public String toString() {
-		return "Employee [name=" + name + ", rate=" + rate + ", hours=" + hours + ", Include bonuses=" + (getBonuses() + getSalary())
-				+ "]";
+		return "Employee [name = " + name + ", rate = " + rate + ", hours = " + hours + ", Salary = " + getSalary()
+				+ " $" + ", Include bonuses = " + (getBonuses() + getSalary()) + " $" + "]";
 	}
-	
+
 }
