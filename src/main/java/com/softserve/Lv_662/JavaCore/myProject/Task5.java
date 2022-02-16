17176ca4cdafbfb0c0ef4e51442b54e5d2b5ad7d
Serhii Okhota <sergiy.okhota@gmail.com>
2022-02-16 17:47:25 +0200

    add Task 5,7

---
+++ src/main/java/com/softserve/Lv_662/JavaCore/myProject/Task5.java
@@ -0,0 +1,30 @@
+package PracticalTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task5 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = null;
+        System.out.println("Enter word to check is it palindrome");
+        try {
+            input = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        isPalindrome(input);
+
+    }
+    public static void isPalindrome(String input){
+        int i = 0, j = input.length() - 1;
+        while (i < j) {
+            if (input.charAt(i) != input.charAt(j))
+                System.out.println("Word is not a palindrome");  ;
+            i++;
+            j--;
+        }
+        System.out.println("Word is a palindrome");;
+    }
+}
---
+++ src/main/java/com/softserve/Lv_662/JavaCore/myProject/Task7.java
@@ -0,0 +1,26 @@
+package PracticalTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task7 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = 1;
+        System.out.println("Enter number to get factorial");
+        try {
+            input = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            System.out.println("Wrong input, please enter number only");
+        }
+        getFactorial(input);
+    }
+    public static void getFactorial(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        System.out.println("Factorial of your input = " + result );
+    }
+}
